2751daef2f80f4499975e5226764591766400682
Nanai10a <Nanai10a@tutanota.com>
2021-01-15 13:54:40 +0900

    :sparkles: 環境構築完了.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import * as Next from "next";
+import * as Material_UI from "@material-ui/core";
 
 type IndexProps = Record<string, never>;
 
 const Index: React.FC<IndexProps> = (props: IndexProps): React.ReactElement => {
-  return <React.Fragment />;
+  return (
+    <Material_UI.Button variant={"contained"} color={"primary"}>
+      hi, Material-ui.
+    </Material_UI.Button>
+  );
 };
 
 export const getStaticProps: Next.GetStaticProps<IndexProps> = async (): Promise<{
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
+import * as fs from "fs";
+import path from "path";
+
 import * as Fastify from "fastify";
+import * as Next from "next";
 
-const fastify = Fastify.fastify();
+(async (): Promise<void> => {
+  const fastify = Fastify.fastify({
+    logger: {
+      prettyPrint: true,
+    },
+    https: {
+      key: fs.readFileSync(path.join(__dirname, "../private/key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, "../private/cert.pem")),
+    },
+  });
 
-const run = async (): Promise<void> => {
+  const next = Next.default({ dev: process.env.NODE_ENV === "development" });
+  await next.prepare();
+  const next_handler = await next.getRequestHandler();
+
+  fastify.get("*", (req, res) => {
+    next_handler(req.raw, res.raw);
+  });
+
   await fastify.listen(3000, "0.0.0.0");
-};
-
-run().catch((reason) => {
+})().catch((reason) => {
   console.error(reason);
   process.exit(1);
 });
